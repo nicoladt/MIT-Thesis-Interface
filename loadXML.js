25f19a9fc91824d9c63f3ec925370c8f58b93ccd
Ewald Zietsman <ewald.zietsman@gmail.com>
2013-07-18 21:30:13 +0200

    Annotations loaded into html from xml file

--- loadXML.js
+++ loadXML.js
@@ -12,15 +12,45 @@ $.ajax({
         $(xml).find("annotation").each( function (index){
             url = $(this).find('url');
             username = $(this).find('username');
-            datetime = $(this).find('datetime');
+            datetime = $(this).find('annotation>datetime');
             highlighted = $(this).find('highlighted');
-            comment = $(this).find('comment');
+            comment = $(this).find('annotation>comment');
+            anntype = $(this).attr('type');
+            replies = $(this).find('reply');
 
-            console.log(url.text());
-            console.log(username.text());
-            console.log(datetime.text());
-            console.log(highlighted.text());
-            console.log(comment.text());
+            table = $('.annotation-table');
+            
+            row = $('<tr></tr>');
+
+            // type column only gets the type
+            typeentry = $('<td></td>');
+            typeentry.html(anntype);
+            
+            // annotation info gets 3 divs
+            annentry = $('<td class="annotation-entry"></td>');
+            urldiv = $('<div class="url"/>').html('<a href="' + url.text() + '">' + url.text() + "</a>");
+            hldiv = $('<div class="highlighted"/>').html(highlighted.text());
+            commentdiv = $('<div class="comment"/>').html(comment.text());
+
+            annentry.append(urldiv);
+            annentry.append(hldiv);
+            annentry.append(commentdiv);
+            
+            // number of replies
+            replyentry = $('<td class="replies-entry"/>');
+            replyentry.html(replies.length);
+
+            // date
+            dateentry = $('<td class="date-entry"/>');
+            dateentry.html(datetime.text());
+            
+            // add everything to table
+            row.append(typeentry);
+            row.append(annentry);
+            row.append(replyentry);
+            row.append(dateentry);
+            table.append(row);
+
         });
 }
  });
